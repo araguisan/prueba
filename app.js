c0b29b90f2b9be2c3a5c554ad0e55d4724a9906d
araguisan <martinandresxd@gmail.com>
2022-09-15 16:42:55 -0500

    use por primera ves js

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log(123)
+let x=14
+x =90
+var y=20
+console.log(x+y)
+
+function sumTwoNomberss(){
+    return 4+5
+}
+
+
+function changeContent(){
+    document.getElementById("toChange").innerHTML = "cambiado desde javascript"
+}
+
+console.log(sumTwoNomberss())
+changeContent()
