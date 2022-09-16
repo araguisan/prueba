d0555a435624982a2bc485f853add9508fb68d69
araguisan <martinandresxd@gmail.com>
2022-09-15 20:40:58 -0500

    implemente botones en html y con java script le di funcionas a cada uno de los botones implementando la base de datos de apex oracle

--- app.js
+++ app.js
@@ -11,6 +11,113 @@ function sumTwoNomberss(){
 function changeContent(){
     document.getElementById("toChange").innerHTML = "cambiado desde javascript"
 }
+function addResponseData(){
+    //elemento del DOM -> document element object
+    const $respnseContainer= document.getElementById("response");
+    //$respnseContainer.innerHTML="texto agregado desde javaScript"+"<br>";
+    $.ajax({
+        url:"https://ga6ae41cae65926-vogwkjxu2344xt5x.adb.us-phoenix-1.oraclecloudapps.com/ords/admin/api/costumes",
+        type:"GET",
+        datatype:"JSON",
+        success:function(response){
+            console.log(response)
+            console.log(response.count)
+            console.log(response.items)
+            for(let x=0;x<response.items.length;x++){
+                const costume =response.items[x];
+                $respnseContainer.innerHTML+=`${costume.name} ${costume.description} ${costume.year} ${costume.brand} ${costume.idcategory}  <br>`;
+            }
+
+        }
+
+    })
+}
+function sendCostume(){
+    let dataToSend = {
+    "idcostume": $("#id").val(),
+    "name":$("#name").val(),
+    "brand":$("#brand").val(),
+    "year": $("#year").val(),
+    "description":$("#description").val(),
+    "idcategory": $("#idcategory").val()
+    }
+    dataToSend=JSON.stringify(dataToSend);
+  
+    const settings = {
+        "url": "https://ga6ae41cae65926-vogwkjxu2344xt5x.adb.us-phoenix-1.oraclecloudapps.com/ords/admin/api/costumes",
+        "method": "POST",
+        "timeout": 0,
+        "headers": {
+          "Content-Type": "application/json"
+        },
+        "data": dataToSend
+        
+      };
+      
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+      });
+
+    
+}
+function senUpdate(){
+    let dataToSend = {
+    "idcostume": $("#id").val(),
+    "name":$("#name").val(),
+    "brand":$("#brand").val(),
+    "year": $("#year").val(),
+    "description":$("#description").val(),
+    "idcategory": $("#idcategory").val()
+    }
+    dataToSend=JSON.stringify(dataToSend);
+  
+    const settings = {
+        "url": "https://ga6ae41cae65926-vogwkjxu2344xt5x.adb.us-phoenix-1.oraclecloudapps.com/ords/admin/api/costumes",
+        "method": "PUT",
+        "timeout": 0,
+        "headers": {
+          "Content-Type": "application/json"
+        },
+        "data": dataToSend
+        
+      };
+      
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+      });
+
+    
+}
+function sendDelete(){
+    let dataToSend = {
+    "idcostume": $("#id").val(),
+    }
+    dataToSend=JSON.stringify(dataToSend);
+  
+    const settings = {
+        "url": "https://ga6ae41cae65926-vogwkjxu2344xt5x.adb.us-phoenix-1.oraclecloudapps.com/ords/admin/api/costumes",
+        "method": "DELETE",
+        "timeout": 0,
+        "headers": {
+          "Content-Type": "application/json"
+        },
+        "data": dataToSend
+        
+      };
+      
+      $.ajax(settings).done(function (response) {
+        console.log(response);
+      });
+
+    
+}
 
 console.log(sumTwoNomberss())
 changeContent()
+addResponseData()
+
+console.log(sumTwoNomberss())
+changeContent()
+addResponseData()
+
+
